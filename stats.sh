91130a29ee3e9ebbed9e8bf5483ffba658a833b7
Chuck Forsyth <chuck.forsyth@gmail.com>
2016-02-07 23:15:41 -0500

    added script to give some stats on the linux system. needs sysstat installed.

---
+++ stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "*********** Begin IO Stat ************"
+
+/bin/sh -c "iostat 1 3 | tail -7"
+echo ""
+echo "*********** Begin Free Mem ***********"
+echo ""
+free -m
+echo ""
+echo "*********** Begin DF ***********"
+echo ""
+df -h
+echo ""
+echo "*********** Begin Netstat ***********"
+echo ""
+netstat -at
+echo ""
+echo "*********** Begin SAR ***********"
+echo ""
+sar -n TCP
