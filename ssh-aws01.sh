67e740084755bece0119ede65782df344872d0a1
Chuck Forsyth <chuck.forsyth@gmail.com>
2016-02-08 20:18:27 -0500

    two additional connection scripts to connect to other EC2 nodes of mine.

---
+++ ssh-aws01.sh
@@ -0,0 +1,1 @@
+ssh -i "/home/pi/keys/myKeys.pem" ec2-user@ec2-52-90-52-199.compute-1.amazonaws.com
---
+++ ssh-aws02.sh
@@ -0,0 +1,1 @@
+ssh -i "/home/pi/keys/myKeys.pem" ec2-user@ec2-54-165-225-191.compute-1.amazonaws.com
