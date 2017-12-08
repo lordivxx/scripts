3230c9487102b27aaaf0e475bf66ca4841a87246
forsythc <forsythc>
2017-12-07 19:42:02 -0800

    lazy commit - bad

--- install-home-assistant.sh
+++ install-home-assistant.sh
@@ -2,7 +2,7 @@
 
 yum install -y python-virtualenv python3
 #rm -rf $HOME/virtualenvs/home-assistant
-virtualenv --system-site-packages --download -p python3 /home/forsythc/virtualenvs/home-asssistant
+virtualenv --system-site-packages -p python3 /home/forsythc/virtualenvs/home-asssistant
 #source /home/forsythc/virtualenvs/home-asssistant/bin/activate
 /$HOME/virtualenvs/home-asssistant/bin/pip3 install homeassistant
 #~/virtualenvs/home-asssistant/bin/python3 /home/forsythc/virtualenvs/home-asssistant/bin/hass --open-ui
