de2f2b6b430e58897f19f424c7b6d2401887799c
forsythc <forsythc>
2018-07-26 21:45:33 -0700

    add backup-self-jenkins-docker.sh

--- backup-self-jenkins-docker.sh
+++ backup-self-jenkins-docker.sh
@@ -1,1 +1,1 @@
-cd /var/jenkins_home; tar -C /var/lib/docker/volumes/jenkins-data/_data/ -zcvf /home/Documents/jenkins-docker-backup/jenkins-docker.tar.gz . 
+cd /var/jenkins_home; tar -zcvf /home/Documents/jenkins-docker-backup/jenkins-docker.tar.gz --transform 's,^\.,/var/lib/docker/volumes/jenkins-data/_data/,' . 
