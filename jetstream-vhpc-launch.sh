6544ca7976737a501c217bf41ed21330c19e2efd
Chuck Fosyth <forsythc@ucr.edu>
2022-03-19 23:07:13 -0700

    update

--- jetstream-vhpc-launch.sh
+++ jetstream-vhpc-launch.sh
@@ -64,6 +64,11 @@ virsh pool-create-as --name vhpc --type dir --target /vhpc/storage-pool --print-
 virsh pool-define vhpc-storage-pool-shorter.xml && virsh pool-start vhpc && virsh pool-autostart vhpc
 
 firewall-cmd --permanent --zone=libvirt --add-port=2049/tcp && firewall-cmd --permanent --zone=libvirt --add-port=2049/udp && firewall-cmd --permanent --zone=libvirt --add-port=111/tcp && firewall-cmd --permanent --zone=libvirt --add-port=111/udp
+firewall-cmd --permanent --zone=libvirt --add-service=nfs
+firewall-cmd --permanent --zone=libvirt --add-service=nfs3
+firewall-cmd --permanent --zone=libvirt --add-service=rpc-bind
+systemctl reload firewalld
+
 
 #virsh vol-create-as --pool vhpc --name n1 --capacity 10G --format qcow2
 #virsh vol-create-as --pool vhpc --name n2 --capacity 10G --format qcow2
@@ -84,7 +89,11 @@ pip install gdown
 
 gdown https://drive.google.com/drive/folders/1170b6nraaleesZqaLksft6XauyoZ96pZ?usp=sharing -O ./ --folder
 
-cd jetstream-vhpc && tar -zxvf n1.tar.gz && cd ..
+rm -f /etc/exports.d/cockpit-file-sharing.exports
+cp -f jetstream-vhpc/cockpit-file-sharing.exports /etc/exports.d/cockpit-file-sharing.exports
+systemctl restart nfs-server.service
+
+cd jetstream-vhpc && tar -zxvf n1.tar.gz && rm -f n1.tar.gz && cd ..
 
 cp jetstream-vhpc/n1 /vhpc/storage-pool/n1
 cp jetstream-vhpc/n1 /vhpc/storage-pool/n2
@@ -104,21 +113,33 @@ virsh start n3
 
 sleep 30
 
-cp jetstream-vhpc/vhpc /root/.ssh/vhpc
+rm -f /root/.ssh/vhpc
+cp -f jetstream-vhpc/vhpc /root/.ssh/vhpc
 chmod 400 /root/.ssh/vhpc
-cp jetstream-vhpc/vhpc.pub /root/.ssh/vhpc.pub
-cp jetstream-vhpc/config /root/.ssh/config
-cat .ssh/vhpc.pub >> .ssh/authorized_keys
-cp jetstream-vhpc/known_hosts /root/.ssh/known_hosts
 
-cp jetstream-vhpc/slurm.conf /etc/slurm/slurm.conf
+rm -f /root/.ssh/vhpc.pub
+cp -f jetstream-vhpc/vhpc.pub /root/.ssh/vhpc.pub
+
+rm -f /root/.ssh/config
+cp -f jetstream-vhpc/config /root/.ssh/config
+
+cat .ssh/vhpc.pub >> .ssh/authorized_keys
+
+rm -f /root/.ssh/known_hosts
+cp -f jetstream-vhpc/known_hosts /root/.ssh/known_hosts
+
+rm -f /etc/slurm/slurm.conf
+cp -f jetstream-vhpc/slurm.conf /etc/slurm/slurm.conf
 sed -i "s/js-16-133.jetstream-cloud.org/$(hostname)/g" /etc/slurm/slurm.conf
 
+rm -f /etc/munge/munge.key
 cp -f jetstream-vhpc/munge.key /etc/munge/munge.key
+chown munge: /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
 
 for x in n1 n2 n3; do ssh $x hostnamectl set-hostname $x; done
 for x in n1 n2 n3; do scp /etc/slurm/slurm.conf $x:/etc/slurm/slurm.conf ; done
+#for x in n1 n2 n3; do scp /etc/munge/munge.key $x:/etc/munge/munge.key ; done
 pdsh -w n[1-3] "systemctl restart slurmd"
 
 systemctl enable munge
